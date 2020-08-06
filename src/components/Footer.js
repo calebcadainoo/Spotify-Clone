0b6e8ec2953c78daf6fd87cbdb2da72aa1229020
Caleb Crosby Adainoo <56970927+calebcadainoo@users.noreply.github.com>
2020-08-06 09:21:24 +0000

    halfway through will resume soon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../css/Footer.css'
+import PlayCircelOutlineIcon from '@material-ui/icons/PlayCircleOutline'
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious'
+import SkipNextIcon from '@material-ui/icons/SkipNext'
+import ShuffleIcon from '@material-ui/icons/Shuffle'
+import RepeatIcon from '@material-ui/icons/Repeat'
 
 function Footer() {
 	return (
 		<div className='footer'>
-			I am the footer
+			<div className='footer-left'>
+				<p>album info</p>
+			</div>
+
+			<div className='footer-center'>
+				<ShuffleIcon className='footer-green' />
+				<SkipPreviousIcon className='footer-icon' />
+				<PlayCircelOutlineIcon fontSize='large' className='footer-green' />
+				<SkipNextIcon className='footer-icon' />
+				<RepeatIcon className='footer-green' />
+			</div>
+
+			<div className='footer-right'>
+				jaja
+			</div>
 		</div>
 	)
 }
